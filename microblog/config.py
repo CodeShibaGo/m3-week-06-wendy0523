4d74a9661e7c050a7c41edb8b7e9c832253c67a7
lin <shower112024@gmail.com>
2024-03-08 17:56:13 +0800

    SQLAlchemy mysql

--- microblog/config.py
+++ microblog/config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
+class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://root:root@localhost:5000/data'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
