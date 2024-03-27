95d734dbc159d58d388c4f2abd273fcb0ad47ccf
lin <shower112024@gmail.com>
2024-03-27 11:21:04 +0800

    User Avatars

--- microblog/app/model.py
+++ microblog/app/model.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
+from hashlib import md5
 
 class User(UserMixin,db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -16,6 +17,9 @@ class User(UserMixin,db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     phone_number: so.Mapped[Optional[str]] = so.mapped_column(sa.String(10))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
@@ -34,6 +38,10 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().
+encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
 @login.user_loader
 def load_user(id):
